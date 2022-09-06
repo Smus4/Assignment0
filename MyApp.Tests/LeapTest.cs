0243194a55fc3081e9c27bd895d32e2c38eb10b6
Smus4 <raoo@itu.dk>
2022-09-06 14:28:42 +0200

    Implemented error handling.

--- MyApp.Tests/LeapTest.cs
+++ MyApp.Tests/LeapTest.cs
@@ -1,27 +1,68 @@
-namespace MyApp;
+namespace MyApp.Tests;
 using System;
+using System.IO;
+using System.Text;
 
 public class LeapTest{
 
-
     [Fact]
-    public void is_the_year_a_leap_year()
+    public void is_2000_a_leap_year()
     {
-
         //Arrange
-        var leap = new Leap();
-        var input = Console.ReadLine();
-
-        if(!int.TryParse(input, out _)){
-           Environment.Exit(0);
-        }
-        var year = Int32.Parse(input);
-        
         //Act
-        var result = leap.IsLeapYear(year);
+        var result = Leap.IsLeapYear(2000);
 
         //Assert
-        result.Should().Be("yay");            
+        result.Should().Be(true);            
+    }
+
+    [Fact]
+    public void is_1600_a_leap_year()
+    {
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        Leap.IsLeapYearString("1600");
+
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("yay");                
+    }
+
+    [Fact]
+    public void is_hej_a_leap_year()
+    {
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        Leap.IsLeapYearString("hej");
+
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("Please input a valid number and try again");                
+    }
+
+    [Fact]
+    public void is_1581_a_leap_year()
+    {
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        Leap.IsLeapYearString("1581");
+
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("Please input a year after year 1581");                
+    }
+
+
+    [Fact]
+    public void is_the_year_a_leap_year_hardcode_true()
+    {
+        //Arrange
+        var leap = new Leap();        
+
+        //Act
+        var result0 = Leap.IsLeapYear(2012);
         
+        //Assert
+        result0.Should().Be(true);       
     }
 }
--- MyApp/Leap.cs
+++ MyApp/Leap.cs
@@ -1,25 +1,60 @@
 namespace MyApp;
-
+using System;
 
 public class Leap{
 
+   public static bool IsLeapYear(int year){
 
-   public string IsLeapYear(int year){
-    if(year < 1582){
-        return "Error your input msut be higher than or equal to 1582";
-    }
-        if(year % 4==0){
-            if(year % 100 == 0){
-                if(year % 400 == 0){
-                    return "yay";
-                }
-                return "nay";
+    if(year % 4==0){
+        if(year % 100 == 0){
+            if(year % 400 == 0){
+                return true;
             }
-            return "yay";
-        }else{
-            return "nay";
+                return false;
+            }
+            return true;
+        } else {
+            return false;
         }
     }
+
+    public static void IsLeapYearString(String input){
+
+        try{
+            var year = Int32.Parse(input);
+            if(year < 1582){
+                Console.WriteLine("Please input a year after year 1581");
+            }
+            else if(IsLeapYear(year)){    
+                Console.WriteLine("yay");
+            } else {
+                Console.WriteLine("false");
+        }
+        } catch {
+            Console.WriteLine("Please input a valid number and try again");
+        }  
+        
+    }
+
+
+    // public static void IsLeapYearInput(){
+
+    //     Console.WriteLine("Input a number please: ");
+    //     var input = Console.ReadLine();
+
+    //     // Assert.Throws();
+
+    //     try{
+    //         var year = Int32.Parse(input);
+    //         if(IsLeapYear(year)){
+    //             Console.WriteLine("yay");
+    //         } else {
+    //             Console.WriteLine("false");
+    //     }
+    //     } catch (Exception e){
+    //         Console.WriteLine("Please input a valid number and try again");
+    //     }
+    // }
 
 }
 
